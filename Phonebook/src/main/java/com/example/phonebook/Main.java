d429cac7824781c49a5df9b13b368ce0f48a4344
Vladilav Yepifanov <evn1999@yandex.ru>
2019-02-18 12:43:48 +0300

    Phonebook project init

---
+++ Phonebook/src/main/java/com/example/phonebook/Main.java
@@ -0,0 +1,8 @@
+package com.example.phonebook;
+
+class DBController {
+}
+
+public class Main {
+
+}
---
+++ Phonebook/src/test/java/com/example/phonebook/SmartListTest.java
@@ -0,0 +1,12 @@
+package com.example.phonebook;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import java.lang.reflect.InvocationTargetException;
+
+
+public class DBTest {
+}
