7aa8cf77d4a4f38b08ff82ab4c7f8172bfe92df1
Vladilav Yepifanov <evn1999@yandex.ru>
2019-03-04 18:20:06 +0300

    toGenericString version

---
+++ Reflector/src/main/java/com/example/reflector/Reflector.java
@@ -0,0 +1,121 @@
+package com.example.reflector;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+
+// TODO SecurityException
+// TODO deal with arrays, intefaces, etc
+public class Reflector {
+    public static void printStructure(Class<?> someClass) throws IOException {
+        try (var writer = new BufferedWriter(new FileWriter(someClass.getName() + ".java"))) {
+            ReflectorPrinter.printStructure(someClass, writer, 0);
+        }
+    }
+
+    public static void actuallyPrintStructure(Class<?> someClass) throws IOException {
+        try (var writer = new PrintWriter(System.out)) {
+            ReflectorPrinter.printStructure(someClass, writer, 0);
+        }
+    }
+
+    private static class ReflectorPrinter {
+        Class<?> printedClass;
+        Writer writer;
+        int tabCount;
+
+        public static void printStructure(Class<?> someClass, Writer writer, int tabCount) throws IOException {
+            new ReflectorPrinter(someClass, writer, tabCount).writeStructure();
+
+        }
+
+        private ReflectorPrinter(Class<?> someClass, Writer writer, int tabCount) {
+            this.writer = writer;
+            this.printedClass = someClass;
+            this.tabCount = tabCount;
+        }
+
+        private void writeTabs() throws IOException {
+            for (int i = 0; i < tabCount; i++) {
+                writer.append('\t');
+            }
+        }
+
+        private void writeLn(String string) throws IOException {
+            writeTabs();
+            writer.write(string + "\n");
+        }
+
+        private void writeStructure() throws IOException {
+            writeClassNameLine();
+            tabCount++;
+
+            writeFields();
+            writeMethods();
+            writeSubclasses();
+
+            tabCount--;
+            writeLn("}");
+        }
+
+        private void writeClassNameLine() throws IOException {
+            writeTabs();
+            writeLn(printedClass.getName());
+            writeLn(printedClass.getCanonicalName());
+            writeLn(printedClass.getPackageName());
+            writeLn(printedClass.getSimpleName());
+            writeLn(printedClass.getTypeName());
+            writeLn(printedClass.toString());
+            writeLn(printedClass.toGenericString() + " {");
+        }
+
+        private void writeFields() throws IOException {
+            Field[] fields = printedClass.getDeclaredFields();
+            for (Field i : fields) {
+                writeLn(i.toGenericString() + ";");
+            }
+            writeLn("");
+        }
+
+        private void writeSubclasses() throws IOException {
+            Class<?>[] subclasses = printedClass.getDeclaredClasses();
+            for (Class<?> i : subclasses) {
+                printStructure(i, writer, tabCount);
+            }
+            writeLn("");
+        }
+
+
+        private void writeMethods() throws IOException {
+            Method[] methods = printedClass.getDeclaredMethods();
+            for (Method i : methods) {
+                writeLn(i.toGenericString() + "{");
+                writeLn("\tthrow new NotImplementedException();"); // TODO write smth else?
+                writeLn("}");
+            }
+            writeLn("");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            actuallyPrintStructure(Reflector.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static private class Kek<T> {
+        T bob;
+        T Kek;
+
+        Kek Kek() {
+            return null;
+        }
+        <Y> void keklol(Y y, T t) {
+
+        }
+    }
+}
---
+++ Reflector/src/test/java/com/example/reflector/ReflectorTest.java
@@ -0,0 +1,5 @@
+package com.example.reflector;
+
+public class ReflectorTest {
+
+}
