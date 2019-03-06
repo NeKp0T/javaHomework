e2164979d6ab50b77597619efa175e6a74ec6779
Vladilav Yepifanov <evn1999@yandex.ru>
2019-03-06 15:36:41 +0300

    init project

---
+++ Cv2/src/main/java/com/example/cv2/Injector.java
@@ -0,0 +1,14 @@
+package com.example.cv2;
+
+import java.util.List;
+
+public class Injector {
+
+    /**
+     * Create and initialize object of `rootClassName` class using classes from
+     * `implementationClassNames` for concrete dependencies.
+     */
+    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
+        throw new IllegalStateException();
+    }
+}
---
+++ Cv2/src/test/java/com/example/cv2/TestInjector.java
@@ -0,0 +1,46 @@
+package com.example.cv2;
+
+import com.example.cv2.testClasses.ClassWithOneClassDependency;
+import com.example.cv2.testClasses.ClassWithOneInterfaceDependency;
+import com.example.cv2.testClasses.ClassWithoutDependencies;
+import com.example.cv2.testClasses.InterfaceImpl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+
+
+public class TestInjector {
+
+    @Test
+    public void injectorShouldInitializeClassWithoutDependencies()
+            throws Exception {
+        Object object = Injector.initialize("task.testClasses.ClassWithoutDependencies", Collections.emptyList());
+        assertTrue(object instanceof ClassWithoutDependencies);
+    }
+
+    @Test
+    public void injectorShouldInitializeClassWithOneClassDependency()
+            throws Exception {
+        Object object = Injector.initialize(
+                "task.testClasses.ClassWithOneClassDependency",
+                Collections.singletonList("task.testClasses.ClassWithoutDependencies")
+        );
+        assertTrue(object instanceof ClassWithOneClassDependency);
+        ClassWithOneClassDependency instance = (ClassWithOneClassDependency) object;
+        assertTrue(instance.dependency != null);
+    }
+
+    @Test
+    public void injectorShouldInitializeClassWithOneInterfaceDependency()
+            throws Exception {
+        Object object = Injector.initialize(
+                "task.testClasses.ClassWithOneInterfaceDependency",
+                Collections.singletonList("task.testClasses.InterfaceImpl")
+        );
+        assertTrue(object instanceof ClassWithOneInterfaceDependency);
+        ClassWithOneInterfaceDependency instance = (ClassWithOneInterfaceDependency) object;
+        assertTrue(instance.dependency instanceof InterfaceImpl);
+    }
+}
