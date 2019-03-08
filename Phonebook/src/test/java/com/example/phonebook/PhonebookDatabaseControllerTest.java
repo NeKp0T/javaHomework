957963add577fedaec82b2628db9464ec1245419
Vladilav Yepifanov <evn1999@yandex.ru>
2019-03-08 13:57:11 +0300

    made controller field in test class private

--- Phonebook/src/test/java/com/example/phonebook/PhonebookDatabaseControllerTest.java
+++ Phonebook/src/test/java/com/example/phonebook/PhonebookDatabaseControllerTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class PhonebookDatabaseControllerTest {
 
-    PhonebookDatabaseController db;
+    private PhonebookDatabaseController db;
 
     private static final Entry[] INITIAL_ENTRIES = {
             new Entry(1, "aaa", "000"),
