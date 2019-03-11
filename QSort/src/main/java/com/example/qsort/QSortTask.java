801296851e92b0a0964fc108219cd491e5952329
Vladilav Yepifanov <evn1999@yandex.ru>
2019-03-11 20:36:13 +0300

    QSort working version

---
+++ QSort/src/main/java/com/example/qsort/QSortTask.java
@@ -0,0 +1,48 @@
+package com.example.qsort;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+
+import static com.example.qsort.QSort.partition;
+
+public class QSortTask<T extends Comparable<T>> implements Runnable {
+    final List<T> list;
+    final CountDownLatch latch;
+    final ExecutorService threadPool;
+
+    QSortTask(@NotNull List<T> list, @NotNull CountDownLatch latch, @NotNull ExecutorService threadPool) {
+        this.list = list;
+        this.latch = latch;
+        this.threadPool = threadPool;
+    }
+
+    @Override
+    public void run() {
+        if (list.size() <= 1) {
+            if (list.size() == 1) {
+                latch.countDown();
+            }
+            return;
+        }
+
+        // pick middle because random is slow, and this strategy works best for already sorted lists
+        T base = list.get(list.size() / 2);
+
+        int middlePosition = partition(list, base);
+        List<T> leftPart = list.subList(0, middlePosition);
+        List<T> rightPart = list.subList(middlePosition + 1, list.size());
+        latch.countDown();
+
+        threadPool.submit(getSortListTask(leftPart));
+        threadPool.submit(getSortListTask(rightPart));
+    }
+
+     private Runnable getSortListTask(List<T> listToSort) {
+        return new QSortTask<>(listToSort, latch, threadPool);
+    }
+}
+
---
+++ QSort/src/test/java/com/example/qsort/QSortTest.java
@@ -0,0 +1,127 @@
+package com.example.qsort;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QSortTest {
+    private ArrayList<Integer> list;
+    private Random randomSource;
+
+    @BeforeEach
+    void init() throws NoSuchMethodException {
+        list = new ArrayList<>();
+        randomSource = new Random(533);
+    }
+
+    private void fillList(int size) {
+        list.addAll(randomSource.ints().limit(size).boxed().collect(Collectors.toList()));
+    }
+
+    private <T> int callPartition(List<T> listToPartition, T base) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method[] methods = QSort.class.getDeclaredMethods();
+        Method partitionMethod = null;
+        for (Method method : methods) {
+            if (method.getName().equals("partition")) {
+                partitionMethod = method;
+            }
+        }
+//        Method partitionMethod = QSort.class.getMethod("partition", List.class, Comparable.class);
+        partitionMethod.setAccessible(true);
+        return (int) partitionMethod.invoke(null, listToPartition, base);
+    }
+
+    private <T extends Comparable<T>> void checkPartitionCorrectness(List<T> listToTest, T base, int pos) {
+        listToTest.subList(0, pos).forEach(element -> assertTrue(element.compareTo(base) <= 0));
+        listToTest.subList(pos, listToTest.size()).forEach(element -> assertTrue(element.compareTo(base) >= 0));
+        assertEquals(0, base.compareTo(listToTest.get(pos)));
+    }
+
+    private <T extends Comparable<T>> void checkPartition(List<T> listToTest, T base) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        List<T> copyOfList = new ArrayList<>(listToTest);
+        checkPartitionCorrectness(listToTest, base, callPartition(listToTest, base));
+        copyOfList.sort(Comparator.naturalOrder());
+        listToTest.sort(Comparator.naturalOrder());
+        assertEquals(copyOfList, listToTest);
+    }
+
+    @Test
+    void somePartitionTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        list.addAll(List.of(1, 5, 3, 4, 2));
+        checkPartition(list, 3);
+    }
+
+    @Test
+    void otherPartitionTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        list.addAll(List.of(2, 1, 3));
+        checkPartition(list, 1);
+    }
+
+    @Test
+    void sortedPartitionTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));
+        checkPartition(list, 5);
+        list = new ArrayList<>(List.of(9,8,7,6,5,4,3,2,1));
+        checkPartition(list, 5);
+    }
+
+    @Test
+    void firstAndLastElementsPartitionTests() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));
+        checkPartition(list, 1);
+        list = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));
+        checkPartition(list, 9);
+        list = new ArrayList<>(List.of(5,2,4,5,6));
+        checkPartition(list, 2);
+        list = new ArrayList<>(List.of(5,2,4,5,6));
+        checkPartition(list, 6);
+    }
+
+    @Test
+    void repeatingBaseElementTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        list = new ArrayList<>(List.of(1,2,2,1,2,3,2,3,2));
+        checkPartition(list, 2);
+    }
+
+    @Test
+    void randomPartitionTest() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        for (int i = 0; i < 30; i++) {
+            fillList(1000);
+            checkPartition(list, list.get(1));
+        }
+    }
+
+    private <T extends Comparable<T>> void checkSortCorrectness(List<T> listToTest, int threadsCount) throws InterruptedException {
+        List<T> listCopy = new ArrayList<>(listToTest);
+        QSort.parallelSort(list, threadsCount);
+        listCopy.sort(Comparator.naturalOrder());
+        assertEquals(listCopy, listToTest);
+    }
+
+    @Test
+    void OneThreadQSortTest() throws InterruptedException {
+        list = new ArrayList<>(List.of(5,1,4,3,2));
+        checkSortCorrectness(list, 1);
+    }
+
+    @Test
+    void FourThreadQSortTest() throws InterruptedException {
+        list = new ArrayList<>(List.of(5,1,4,3,2));
+        checkSortCorrectness(list, 4);
+    }
+
+    @Test
+    void NegativeThreadQSortTest() {
+        fillList(10);
+        assertThrows(IllegalArgumentException.class, () -> checkSortCorrectness(list, -1));
+    }
+}
