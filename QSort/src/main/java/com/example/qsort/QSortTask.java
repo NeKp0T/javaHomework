946a9775d41ac0f265214543e874988eead6bdd1
Vladilav Yepifanov <evn1999@yandex.ru>
2019-03-16 21:56:12 +0300

    moved partition to QSortTask

--- QSort/src/main/java/com/example/qsort/QSortTask.java
+++ QSort/src/main/java/com/example/qsort/QSortTask.java
@@ -3,6 +3,7 @@ package com.example.qsort;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.concurrent.CountDownLatch;
 
 import static com.example.qsort.QSort.partition;
@@ -29,6 +30,9 @@ class QSortTask<T extends Comparable<T>> implements Runnable {
         this.strategy = strategy;
     }
 
+    /**
+     * Runs qsort on contained list
+     */
     @Override
     public void run() {
         if (list.size() <= 1) {
@@ -51,6 +55,52 @@ class QSortTask<T extends Comparable<T>> implements Runnable {
 
      private QSortTask<T> getSortListTask(List<T> listToSort) {
         return new QSortTask<>(listToSort, latch, strategy);
+    }
+
+
+    /**
+     * Partitions provided list by comparing with base element.
+     * After method execution all elements prior to returned index are guaranteed to be
+     * less or equal, then provided base element and all elements from returned index to the end of list
+     * to be greater or equal then base.
+     * Additionally, If provided list contains at list one value equal to base, returned index
+     * will be occupied by one of them.
+     *
+     * Requires list to contain at least two items.
+     *
+     * @return index, that every element in list before it is lower or equals to base,
+     *          and every element after it is greater or equals to base.
+     */
+    static <T extends Comparable<T>> int partition(@NotNull List<T> list, @NotNull T base) {
+        ListIterator<T> forward = list.listIterator();
+        ListIterator<T> backward = list.listIterator(list.size());
+        T leftItem = forward.next();
+        T rightItem = backward.previous();
+
+        while (true) {
+            while (forward.hasNext() && leftItem.compareTo(base) < 0) {
+                leftItem = forward.next();
+            }
+            while (backward.hasPrevious() && rightItem.compareTo(base) > 0) {
+                rightItem = backward.previous();
+            }
+
+            if (forward.previousIndex() >= backward.nextIndex()) {
+                break;
+            }
+
+            forward.set(rightItem);
+            backward.set(leftItem);
+
+            if (leftItem.compareTo(base) != 0) {
+                leftItem = rightItem;
+                rightItem = backward.previous();
+            } else {
+                rightItem = leftItem;
+                leftItem = forward.next();
+            }
+        }
+        return forward.previousIndex();
     }
 }
 
